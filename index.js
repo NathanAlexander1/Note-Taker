d502432fad1440e71fb14fb7f44dde16607ba3ad
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2022-10-27 17:55:59 -0700

    got delete button working

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 3000;
 // importing path package from standard library
 const path = require("path");
 const fs = require("fs");
+const uuid = require('uuid');
+// console.log(uuid.v4())
+
+const { runInNewContext } = require("vm");
+
 
 //middle ware to serve static assets
 app.use(express.static("public"));
@@ -51,6 +56,7 @@ app.post("/api/notes", (req, res) => {
       });
     } else {
       const dataArr = JSON.parse(data);
+      req.body.id = uuid.v4();
       dataArr.push(req.body);
       fs.writeFile(
         "./db/db.json",
@@ -72,6 +78,56 @@ app.post("/api/notes", (req, res) => {
     }
   });
 });
+
+// app.get("/api/notes/:id", (req, res) => {
+//   console.log("this should retrieve a specific note")
+// })
+
+app.delete("/api/notes/:id", (req, res) => {
+  const forDeletion = req.params.id;
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({
+        msg: "oh no! error!",
+        err: err,
+      });
+    } else {
+      const dataArr = JSON.parse(data);      
+      //what index is the note in the array (find by id)?
+      //remove that index from the array
+      let index = -1;
+      for (let i=0; i<dataArr.length;i++){
+        if (dataArr[i].id === forDeletion) {
+          index = i;
+          break;
+        }
+      }
+      if (index !== -1) {
+        //delete from array
+        dataArr.splice(index,1);
+      }
+      fs.writeFile(
+        "./db/db.json",
+        JSON.stringify(dataArr, null, 4),
+        (err, data) => {
+          if (err) {
+            console.log(err);
+            res.status(500).json({
+              msg: "oh no! error!",
+              err: err,
+            });
+          } else {
+            res.json({
+              msg: "successfully deleted!",
+            });
+          }
+        }
+      );
+    }
+  });
+  
+})
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
